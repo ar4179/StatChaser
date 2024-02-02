60bcf3fb58443f7af0a658f5e908415ae832a78d
aryan <ar4179@columbia.edu>
2024-02-02 00:41:59 -0500

    back to normal

--- homescreen/urls.py
+++ homescreen/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    # path('',views.graph,name='home-page'),
-    path('',views.aboutMe,name='about-me'),
+    path('',views.graph,name='home-page'),
+    # path('',views.aboutMe,name='about-me'),
     path('aboutme',views.aboutMe,name='about-me')
 ]
