cd27607f7ecf1ea4a45f3642342dab93003926bf
aryan <ar4179@columbia.edu>
2024-02-02 00:29:42 -0500

    under maintenance message

--- homescreen/urls.py
+++ homescreen/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('',views.graph,name='home-page'),
+    # path('',views.graph,name='home-page'),
+    path('',views.aboutMe,name='about-me'),
     path('aboutme',views.aboutMe,name='about-me')
 ]
